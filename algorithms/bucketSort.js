8b3996af63e01512a67597dc18a44f8c21ad6b71
Tomara Redina <redtamara1124@gmail.com>
2024-02-18 15:45:27 +0300

    rebuild bucket sort

--- algorithms/bucketSort.js
+++ algorithms/bucketSort.js
@@ -1,18 +1,43 @@
-const bucketSort = arr => {
+function bucketSort(array) {
+  if (array.length === 0) {
+    return array;
+  }
 
-    size = arr.length;
+  // Determine minimum and maximum values
+  var i;
+  var minValue = array[0];
+  var maxValue = array[0];
+  for (i = 1; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i];    // Input Range
+    } else if (array[i] > maxValue) {
+      maxValue = array[i];
+    }
+  }
 
-    const min = Math.min(...arr);
-    const max = Math.max(...arr);
-    const buckets = Array.from(
-      { length: Math.floor((max - min) / size) + 1 },
-      () => []
-    );
-    arr.forEach(val => {
-      buckets[Math.floor((val - min) / size)].push(val);
-    });
-    return buckets.reduce((acc, b) => [...acc, ...b.sort((a, b) => a - b)], []);
-  };
+  // Initialize buckets
+  bucketSize = array.length
+  var bucketCount = Math.floor((maxValue - minValue) / bucketSize) + 1;
+  var buckets = new Array(bucketCount);
+  for (i = 0; i < buckets.length; i++) {
+    buckets[i] = [];
+  }
+
+  // Distribute input array values into buckets
+  for (i = 0; i < array.length; i++) {
+    buckets[Math.floor((array[i] - minValue) / bucketSize)].push(array[i]);
+  }
+
+  // Sort buckets and place back into input array
+  array.length = 0;
+  for (i = 0; i < buckets.length; i++) {
+    buckets[i].sort();
+    for (var j = 0; j < buckets[i].length; j++) {
+      array.push(buckets[i][j]);          
+    }
+  }
+  console.log(array);
+};
 
 // Экспорт функции для использования в других модулях
 module.exports = bucketSort;
